f868fcfadd1fa395ea00bec552d6646d7bf10a26
Sobolev Dmitriy <mityaizhf@rambler.ru>
2019-09-07 17:55:17 +0300

    First commit

---
+++ dict.py
@@ -0,0 +1,7 @@
+dictionary = {
+   "city": "Москва", 
+   "temperature": "20"
+}
+print(dictionary["city"])
+dictionary["temperature"] = int(dictionary["temperature"]) - 5
+print(dictionary["temperature"])
---
+++ price.py
@@ -0,0 +1,9 @@
+#price = 100
+#discount = 5
+#price_with_siscount = price - price * discount/100
+#print(price_with_siscount) 
+
+
+def get_summ(one, two, delimiter='&'):
+    print(f'Результат: {one} {delimiter} {two}')
+        
